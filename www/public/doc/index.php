56b2a3014b90cb11ab8f6b389b182839ce21ddd5
Karel Zavadil <mojemejly@centrum.cz>
2018-01-17 23:54:31 +0100

    doc

---
+++ www/public/doc/index.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="cs">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+		<meta name="description" content="Belbinův test online.">
+		<meta name="author" content="Karel Zavadil">
+		<link rel="icon" href="/favicon.ico">
+
+		<title>Belbinův test týmových rolí - Dokumentace</title>
+
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css">
+		<link rel="stylesheet" href="http://belbin.zavadil.eu/style.css">
+		<link rel="stylesheet" href="http://belbin.zavadil.eu/print-style.css" type="text/css" media="print" />
+		
+		<script>var chart_data = {"datasets":[{"data":[18.57,16.43,12.86,12.14,11.43,8.57,6.43,5,5,3.57],"backgroundColor":["#556B2F","#FF7F50","#A9A9A9","#B8860B","#5F9EA0","#228B22","#483D8B","#DC143C","#CD5C5C","#6495ED"],"borderWidth":0}],"labels":["Stmelova\u010d","Vyhledava\u010d zdroj\u016f","Neutr\u00e1ln\u00ed body","Analytik","Inov\u00e1tor","Dotahova\u010d","Realiz\u00e1tor","Formova\u010d","Specialista","Koordin\u00e1tor"]};</script>
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+
+	<body>
+		
+		<nav class="navbar navbar-expand-md navbar-dark fixed-top">
+			<a href="http://belbin.zavadil.eu/" class="navbar-brand active">Belbinův test online</a>		  
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon">&nbsp;</span>
+			</button>			
+		</nav>
+	
+		<main role="main">
+			<div class="container">
+	
+				<h1 class="page-title display-4">Belbinův test týmových rolí</h1>
+				
+				<hr/>
+		
+				<h2 class="display-5">Dokumentace</h2>
+				
+				<p>
+					Na této stránce jsou k dispozici nejrůznější dokumenty týkající se projektu.
+				</p>
+
+				<h2>zEngine</h2>
+				<p>
+					Aplikace je postavená na frameworku <strong>zEngine</strong>. 
+					Zde je <a href="/index.xhtml">automaticky vygenerovaná dokumenace v nástroji PHPDox</a>.
+				</p>
+				
+				<hr/>
+
+				<div class="row">
+					<div class="col-md-6">
+						<h2>Zadání</h2>
+						<p>
+							Zadání projektu jako od zákazníka.
+							<a href="http://belbin.zavadil.eu/doc/Belbinův test online - Zadání.odt">ODT</a>,
+							<a href="http://belbin.zavadil.eu/doc/Belbinův test online - Zadání.docx">DOCX</a>
+						</p>
+						
+					</div>
+					<div class="col-md-6">					
+						<h2>Uživatelská příručka</h2>
+						<p>
+							Příručka pro správce aplikace.
+							<a href="http://belbin.zavadil.eu/doc/Belbinův test online - Příručka.odt">ODT</a>,
+							<a href="http://belbin.zavadil.eu/doc/Belbinův test online - Příručka.docx">DOCX</a>						
+						</p>	
+					</div>		
+				</div>
+			
+				<div class="row">
+					<div class="col-md-6">
+						<h2>Technická dokumentace</h2>
+						<p>
+							Popis fungování aplikace. Návod na instalaci a konfiguraci.
+							<a href="http://belbin.zavadil.eu/doc/Belbinův test online - Tech. dokumentace.odt">ODT</a>,
+							<a href="http://belbin.zavadil.eu/doc/Belbinův test online - Tech. dokumentace.docx">DOCX</a>						
+						</p>
+					</div>
+					<div class="col-md-6">
+						<h2>Hodnocení</h2>
+						<p>
+							Předvyplněné hodnocení semestrálního projektu.
+							<a href="http://belbin.zavadil.eu/doc/Belbinův test online - Hodnocení.ods">ODT</a>,
+							<a href="http://belbin.zavadil.eu/doc/Belbinův test online - Hodnocení.xlxs">XLSX</a>						
+						</p>
+					</div>		
+				</div>
+				
+			</div>		
+		
+		</main>		
+
+		<footer class="container text-right">
+			<hr>
+			<p>&copy; Karel Zavadil 2018.</p>
+		</footer>
+	
+				
+		<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js"></script>
+
+	</body>	
+</html>
