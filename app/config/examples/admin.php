bfcd664db3c45cbe4a71723ddf724508ac197b16
Karel Zavadil <mojemejly@centrum.cz>
2018-08-06 16:42:54 +0000

    restruct

---
+++ app/config/examples/admin.php
@@ -0,0 +1,14 @@
+<?php
+
+	return [
+
+		'menu' => [
+			['admin/belbin_roles', 'Týmové role'],			
+			['admin/belbin_questions', 'Otázky'],
+			['admin/belbin_tests', 'Výsledky testů'],
+			['admin/customers', 'Customers']
+		],
+		
+		'show_default_menu' => true
+
+	];
