afca1866cc3690a995fa55376c2a63f3dc56d74d
Karel Zavadil <mojemejly@centrum.cz>
2018-09-27 17:08:08 +0000

    removed redundant files

--- public/index.php
+++ public/index.php
@@ -1,7 +1,5 @@
 <?php
 
 	require_once __DIR__ . '/../../zEngine/src/zengine.php';
-
-	$z = new zEngine('../app/');
-	$z->enableModule('app');
+	$z = new zEngine('../app/', ['app']);
 	$z->run();
--- public/resources.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-	require_once __DIR__ . '/../../../zEngine/src/zengine.php';
-	
-	$z = new zEngine('../app/');
-	$z->enableModule('resources');
-
-	$z->run();
