8ba36b60ecde198adbb0a7f57acca69c8c43353c
Karel Zavadil <mojemejly@centrum.cz>
2018-10-02 15:26:26 +0000

    app dir

--- tests/unit/zEngineTest.php
+++ tests/unit/zEngineTest.php
@@ -3,12 +3,10 @@ declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
 
-final class zEngineTest extends TestCase
-{
-    public function testCreateZInstance(): void
-    {
-		$z = new zEngine(__DIR__ . '/../app/');
-		$this->assertNotNull($z);       
-    }
+final class zEngineTest extends TestCase {
+  public function testCreateZInstance(): void {
+	   $z = new zEngine(__DIR__ . '/../../app/');
+		 $this->assertNotNull($z);
+  }
 
 }
