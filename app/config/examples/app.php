b6a96a7500c40c28a0a24032779b27e2baad7069
Karel Zavadil <mojemejly@centrum.cz>
2018-10-08 10:29:16 +0000

    3.5 - paging

--- app/config/examples/app.php
+++ app/config/examples/app.php
@@ -4,13 +4,13 @@
 
 		// this is application version
 		// integer part should be identical with GIT branch name
-		'version' => 3.4,
+		'version' => 3.5,
 
     // required zEngine major version (integer value)
 		'require_z_version' => 3,
 
 		// this is minimum required zEngine version
-		'minimum_z_version' => 3.3,
+		'minimum_z_version' => 3.4,
 
 		// list of modules that will be automatically loaded
 		'modules' => ['resources', 'db', 'jobs', 'i18n', 'auth', 'admin', 'menu', 'forms'],
